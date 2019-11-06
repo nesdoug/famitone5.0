53ee2c194ac28f5b168d738c4e4b2b11687a0160
nesdoug <dougfraker@gmail.com>
2019-11-05 21:57:14 -0500

    minor edit
    minor edit

--- famitone5.s
+++ famitone5.s
@@ -738,9 +738,7 @@ update_sound:
 	beq ch1cut
 	clc
 	adc FT_CH1_NOTE_OFF
-		;.if(FT_PITCH_FIX)  
-		;ora FT_PAL_ADJUST
-		;.endif
+		;removed pal pitch fix **
 	tax
 	lda FT_CH1_PITCH_OFF
 	tay
@@ -793,9 +791,7 @@ ch1cut:
 	beq ch2cut
 	clc
 	adc FT_CH2_NOTE_OFF
-		;.if(FT_PITCH_FIX)
-		;ora FT_PAL_ADJUST
-		;.endif
+		;removed pal pitch fix **
 	tax
 	lda FT_CH2_PITCH_OFF
 	tay
@@ -841,9 +837,7 @@ ch2cut:
 	beq ch3cut
 	clc
 	adc FT_CH3_NOTE_OFF
-		;.if(FT_PITCH_FIX)
-		;ora FT_PAL_ADJUST
-		;.endif
+		;removed pal pitch fix **
 	tax
 	lda FT_CH3_PITCH_OFF
 	tay
